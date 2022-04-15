96c4292e7b4c4fa58ac5b85868a905cb90d035b6
= <=>
2022-04-15 16:46:12 +0900

    basic_graph

---
+++ Assets/scripts/GraphManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraphManager : MonoBehaviour
+{
+    /*
+    static public int Graph_distance = 1;
+    public float s_half;
+    public float center;
+    public float h_width;
+    //h_width.x = center.x + s_half.x
+    */
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+}
---
+++ Assets/scripts/clickEvent.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickEvent : MonoBehaviour
+{
+
+    public GameObject graph;
+    public GameObject instance;
+
+    static public float graph_dist = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        click();
+    }
+
+
+    public void click()
+    {
+
+
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("Left-Clicked");
+
+                if (!instance)
+                {
+                    instance = Instantiate(graph);
+                }
+
+                Vector3 center_p = GetComponent<Transform>().position;
+                //Debug.Log(center_p.x);
+                
+                Vector3 s_half = (GetComponent<Transform>().localScale)/2;
+                //Debug.Log(s_half.x);
+                
+                var dist =(center_p.x + s_half.x + graph_dist);
+                //Debug.Log(dist);
+
+                instance.transform.position = new Vector3(center_p.x + dist, center_p.y, center_p.z);
+                       
+            }
+            
+        }
+
+        else if (Input.GetMouseButtonDown(1))
+        {
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("Right-Clicked");
+
+                if (instance)
+                {
+                    instance.GetComponent<Renderer>().enabled = false;
+                    Destroy(instance);
+                }
+               
+                   
+
+            }
+
+        }
+
+
+    }
+}
